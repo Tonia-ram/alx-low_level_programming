ce12d71e98e8bdabef26d1c45d9b480e72ab5e01
Tonia-ram <tonia.ramphisa@gmail.com>
2023-02-17 19:28:48 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,42 +1,47 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- *main - prints a pair from 00-99 but no repeats (00 01, 00 02, 00 03,...)
- *Return: Always 0 (Success)
+ * main - prints a pair from 00-99 but no repeats (00 01, 00 02, 00 03,...)
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int tens;
-	int ones;
-	int t;
-	int o;
+	int  x, y, z, w, aux;
 
-	for (tens = '0'; tens <= '9'; tens++) /*print first two digit combo*/
+	for (x = '0'; x <= '9'; x++)
 	{
-		for (ones = '0'; ones <= '9'; ones++)
-		{
-			for (t = tens; t <= '9'; t++) /*print second of pair*/
-			{
-				for (o = ones + 1; o <= '9'; o++)
-				{
-					putchar(tens);
-					putchar(ones);
-					putchar(' ');
-					putchar(t);
-					putchar(o);
 
-					if (!((tens == '9' && ones == '8') &&
-					      (t == '9' && o == '9')))
+		for (y = '0' ; y <= '9'; y++)
+		{
+
+			for (z = x ; z <= '9'; z++)
+			{
+				if (z == x)
+				{
+					aux = y + 1;
+				}
+				else
+				{
+					aux = '0';
+				}
+				for (w = aux; w <= '9'; w++)
+				{
+					putchar(x);
+					putchar(y);
+					putchar(' ');
+					putchar(z);
+					putchar(w);
+					if (!(x == '9' && y == '8' && z == '9' && w == '9'))
 					{
 						putchar(',');
 						putchar(' ');
 					}
 				}
-				o = '0';
 			}
 		}
 	}
+
 	putchar('\n');
 
 	return (0);
