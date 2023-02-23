bf453047e54a708bc33b0a828259acb477b74d68
Tonia-ram <tonia.ramphisa@gmail.com>
2023-02-23 09:25:47 -0800

    Task 0

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -1,4 +1,4 @@
-#includie "main.h"
+#include "main.h"
 
 /**
 * main - tests function that prints if integer is positive or negative
@@ -10,6 +10,8 @@ int main(void)
 	int i;
 
 	i = 98;
+
 	positive_or_negative(i);
+
 	return (0);
 }
