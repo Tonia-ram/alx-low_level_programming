5409a947da5d2387617d9703a74060443f6f9c20
Tonia-ram <tonia.ramphisa@gmail.com>
2023-03-08 09:32:52 -0800

    exponentiation

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * factorial - return factorial of a given number.
- * @n: int type number
- * Return: factorial of number
+ * factorial - returns factorial of a given value.
+ * @n: int value
+ * Return: factorial of a number
  */
 int factorial(int n)
 {
