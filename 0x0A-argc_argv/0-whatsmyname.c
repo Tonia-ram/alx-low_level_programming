8f51c85845229dccd6649387e3d410b42b09f1cf
Tonia-ram <tonia.ramphisa@gmail.com>
2023-03-10 17:01:30 -0800

    arguments

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Description: a program that prints its name, followed by a new line
+ * @argument_count: amount of arguments
+ * @argument_values: the array of arguments
+ * Return: 0
+ */
 int main(int argument_count,char *argument_values[])
 { 
 	if (argument_count >= 1)
