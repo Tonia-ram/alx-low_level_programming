e926525e010c3068ecb68f8a484f50f472d9f9e3
Tonia-ram <tonia.ramphisa@gmail.com>
2023-04-04 16:58:21 -0700

    function returns the number of elements in list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,14 +2,13 @@
 #include <stdio.h>
 
 /**
- * print_listint - prints all the elements of a listint_t list
+ * listint_len - prints all the elements of a listint_t list
  * @h: pointer to the head node
  *
  * Return: the number of nodes
  */
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
-	/* Count number of nodes */
 	size_t node = 0;
 
 	while (h)
--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include "lists.h"
+
 /**
  * add_nodeint_end - adds a new node at the end of a listint_t list
  * @head: a pointer to the head node
