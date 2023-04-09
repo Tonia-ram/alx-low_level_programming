e5b67e501be4ae16f1fa9618435087c73af7afa7
Tonia-ram <tonia.ramphisa@gmail.com>
2023-04-09 16:02:01 -0700

    function prints binary representation of num

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_binary - prints the binary representation of a number
  * @n: the number to be printed in binary
@@ -8,7 +9,13 @@ void print_binary(unsigned long int n)
 {
 	unsigned long int bit_op = 1;
 	int bit_point = 0;
-	/* Locate the most significant bit */
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	/* locates the most fitting bit */
 	while ((n >> bit_point) != 0)
 	{
 		bit_point++;
@@ -20,10 +27,6 @@ void print_binary(unsigned long int n)
 		if ((n >> bit_point) & bit_op)
 		{
 			_putchar('1');
-		}
-		else if (!((n >> bit_point) & bit_op) && bit_point != 0)
-		{
-			_putchar('0');
 		}
 		else
 		{
