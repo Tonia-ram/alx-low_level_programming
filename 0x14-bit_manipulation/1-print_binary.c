1b524a87cb0a619818fdf3b48b2314889459aadf
Tonia-ram <tonia.ramphisa@gmail.com>
2023-04-11 17:59:42 -0700

    binary numbers

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,31 +7,26 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int bit_op = 1;
+	unsigned long int bit_op = n, mask = 1;
 	int bit_point = 0;
 
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-	/* locates the most fitting bit */
-	while ((n >> bit_point) != 0)
+	while (bit_op > 0)
 	{
 		bit_point++;
+		bit_op >>= 1;
 	}
-	bit_point--;
-	/* Output binary digits using else if statements */
-	while (bit_point >= 0)
+	bit_point -= 1;
+
+	if (bit_point > 0)
+		mask = mask << bit_point;
+
+	while (mask > 0)
 	{
-		if ((n & (bit_op << bit_point)) != 0)
-		{
+		if (n & mask)
 			_putchar('1');
-		}
 		else
-		{
 			_putchar('0');
-		}
-		bit_point--;
+
+		mask >>= 1;
 	}
 }
