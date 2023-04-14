31ce8fa6bca39d49e2f59e5d873728c5b56ea6a1
Tonia-ram <tonia.ramphisa@gmail.com>
2023-04-14 15:52:16 -0700

    function that reads a text file and prints it

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters to read and print
+ *
+ * Return: number of letters read and printed, or 0 on failure
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+		{
+			int file_desc; /* file descriptor */
+			ssize_t num_read, num_written;
+			char *file_buffer;
+
+			if (!filename)
+			{
+				return (0);
+			}
+			/* Open the file */
+			file_desc = open(filename, O_RDONLY);
+
+			if (file_desc < 0)
+			{
+				return (0);
+			}
+			/* Allocate buffer */
+			file_buffer = malloc(sizeof(char) * letters);
+
+			if (!filename)
+			{
+				return (0);
+			}
+			/* Read the file */
+			num_read = read(file_desc, file_buffer, letters);
+
+			if (num_read < 0)
+			{
+				free(file_buffer);
+				close(file_desc);
+				return (0);
+			}
+			/* Write to STDOUT */
+			num_written = write(STDOUT_FILENO, file_buffer, num_read);
+
+			if (num_written < 0)
+			{
+				free(file_buffer);
+				close(file_desc);
+				return (0);
+			}
+			close(file_desc);
+			return (num_read);
+			}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,10 @@
+C - File I/O
+Environment:
+-Language: C -OS: Ubuntu 20.04 LTS -Compiler: gcc -Style guidelines: Betty style
+
+Description of files:
+0. A program that reads a text file and prints it to the POSIX standard output.
+1. A program that creates a file.
+2. A program that appends text at the end of a file.
+3. A program that copies the content of a file to another file.
+4. A program that displays the information contained in the ELF header at the start of an ELF file.
